2144d4e8194656498435dc5dbba05303d8fa92b8
Philippe Casgrain <github@casgrain.com>
2022-06-04 16:50:43 -0400

    Script de mise-à-jour pour arn.biol

---
+++ update_website_arn.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Update the whole folder to the latest
+cd /Users/legendrepierre/numericalecology
+
+/usr/bin/git pull origin main
+if [ $? -ne 0 ];
+then
+  echo "There was an issue with the git update. Exiting."
+  exit 1
+fi
+
+# Update main website (it's in plain HTML)
+rsync -avz numecol/ /Library/WebServer/Documents/ --delete --exclude Streaming
+
+pushd Reprints
+if [ $? -eq 0 ];
+then
+  rsync -avz . /Library/WebServer/Documents/Reprints/ --exclude .git
+fi
+popd
+
+# Note: folder 'prog' is represented as 'labo' on the website
+pushd prog
+#mkdir -p /Users/numericalecology/Sites/labo
+if [ $? -eq 0 ];
+then
+#  rsync -avz . /Library/WebServer/Documents/labo/ --exclude .git --exclude ".DS_Store" -exclude README.md
+  ditto . /Library/WebServer/Documents/labo
+  rm -rf /Library/WebServer/Documents/labo/.git
+fi
+
